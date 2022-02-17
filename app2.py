be2f5ac228341981cdb4dc370a7916dadad2c4fa
hitoshiishizaka <46650229+hitoshiishizaka@users.noreply.github.com>
2022-02-17 16:15:30 +0900

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import qrcode
+from PIL import Image, ImageFilter
+
+st.title('QRコード自動生成アプリL217')
+
+url = st.text_input('QRコードを生成したいURL：')
+
+img = qrcode.make('https://note.nkmk.me/python-pillow-qrcode/')
+
+if st.button('QRコード生成'):
+    _img = qrcode.make('https://note.nkmk.me/python-pillow-qrcode/')
+    _img.save('qrcode.png')
+    #img = Image.open('qrcode.png')
+    #st.image(img)
+    
